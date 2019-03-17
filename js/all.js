e55e99b8a44fc6ba086f3bb0294164962e7ab194
Yulin Wang <yulin0629@gmail.com>
2019-03-17 21:47:30 +0800

    calcuate button and result indicator works fine now

--- js/all.js
+++ js/all.js
@@ -31,14 +31,8 @@ let records = [
     }
 ];
 
-let record_list = document.querySelector('.records');
-
-function getBMIValue(height, weight) {
-    return weight / ((height/100) * (height/100));
-}
-
 function getBMIState(value) {
-    switch(true) {
+    switch (true) {
         case (value < 18.5):
             return ['underweight', '過輕'];
         case (value < 25):
@@ -54,12 +48,61 @@ function getBMIState(value) {
     }
 }
 
+function getBMIValue(height, weight) {
+    return weight / ((height / 100) * (height / 100));
+}
+
+
+
+let record_list = document.querySelector('.records');
+let calculate_btn = document.getElementById('btn-calculate');
+calculate_btn.addEventListener('click', onCalculateClick);
+let resultIndicator = document.querySelector('.result');
+resultIndicator.addEventListener('click', onCalculateClick);
+
+function onCalculateClick(e) {
+    let inputHeight = parseInt(document.getElementById('height').value);
+    let inputWeight = parseInt(document.getElementById('weight').value);
+    addRecords(inputHeight, inputWeight);
+    updateCalcuateButton(inputHeight, inputWeight);
+    updateList();
+}
+
+function addRecords(height, weight) {
+    let currentDate = new Date();
+    let month = (currentDate.getMonth() + 1) + '';
+    month = month.padStart(2, '0');
+    let date = (currentDate.getDate() + '').padStart(2, '0');
+    let formattedDateStr = `${month}-${date}-${currentDate.getFullYear()}`;
+    let newRecord = { height: height, weight: weight, date: formattedDateStr }
+    records.push(newRecord);
+}
+
+
 updateList();
 
+function updateCalcuateButton(height, weight) {
+    const bmiValue = getBMIValue(height, weight);
+    const [bmiState, bmiDescription] = getBMIState(bmiValue);
+    calculate_btn.classList.add('display-none');
+
+    let resultContainer = document.querySelector('.result');
+    resultContainer.classList.remove('display-none');
+    
+    let colorContainer = document.getElementById('colorContainer');
+    colorContainer.className = '';
+    colorContainer.classList.add(bmiState);
+
+    let description = document.querySelector('.result-description');
+    description.textContent = bmiDescription;
+
+    let bmi_number = document.querySelector('.result-bmi-number');
+    bmi_number.textContent = bmiValue.toFixed(2);
+}
 function updateList() {
     record_list.innerHTML = '';
     records.forEach(record => {
-        let bmiValue = getBMIValue(record.height, record.weight);
+        const bmiValue = getBMIValue(record.height, record.weight);
         const [bmiState, bmiDescription] = getBMIState(bmiValue);
         record_list.innerHTML += `
         <li data-level='normal' class="record-${bmiState}">
